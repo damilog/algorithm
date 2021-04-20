8ff55376ab50344e139b636ebcea396c1b618758
Dami Kim <dami.k.dev@gmail.com>
2021-04-20 21:22:36 +0900

    Feat: section 5 2번 문제 풀이

--- basic/code/basic-1.js
+++
@@ -1,7 +0,0 @@
-function solution(a, b, c) {
-  if (a < b && a < c) return a;
-  if (b < a && b < c) return b;
-  if (c < a && c < b) return c;
-}
-
-console.log(solution(2, 5, 1));
---
+++ basic/code/section4/b-4-1.js
@@ -0,0 +1,17 @@
+//#1 자릿수의 합
+//N개의 자연수가 입력되면 각 자연수의 자릿수의 합을 구하고, 그 합이 최대인 자연수를 출력 하는 프로그램을 작성하세요.
+//자릿수의 합이 같은 경우 원래 숫자가 큰 숫자를 답으로 합니다. 만약 235 와 1234가 동시에 답이 될 수 있다면 1234를 답으로 출력해야 합니다.
+
+function solution(n, arr) {
+  arr.reduce((acc, num) => {
+    let currentNumSum = [...num].reduce((acc, num) => acc + num);
+    if (acc < currentNumSum) {
+      acc = currentNumSum;
+    }
+  }, 0);
+
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
