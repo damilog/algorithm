8f2bb0bc12ee502954b588701441fbd64ec047a2
Dami Kim <dami.k.dev@gmail.com>
2021-04-27 00:17:36 +0900

    Feat: stack으로 구현

--- basic/code/section6/b6-1.js
+++ basic/code/section6/b6-1.js
@@ -2,13 +2,16 @@
 //괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
 //(())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
 function solution(s) {
-  let open, close;
-  const input = [...s];
-  input.forEach(v => {
-    v === "(" ? open++ : close++;
-  });
-
-  return open === close ? "YES" : "NO";
+  const stack = [];
+  for (let value of s) {
+    if (value === "(") stack.push(value);
+    else {
+      if (stack === 0) return "NO";
+      stack.pop();
+    }
+  }
+  if (stack.length > 0) return "NO";
+  return "YES";
 }
 
 let a = "(()(()))(()";
